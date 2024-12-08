a44778cfdd9340b40954d5824de8af4fa598d656
vishnu5104 <vishnus510412@gmail.com>
2024-12-08 19:56:19 +0530

    error fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    plugins: [
-        require("daisyui"),
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  plugins: [
+    //@ts-ignore
+    require('daisyui'),
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        Spiker: {
+          ...require('daisyui/src/theming/themes')['dim'],
+          primary: '#6ba672',
+        },
+        Ghostiny: {
+          ...require('daisyui/src/theming/themes')['emerald'],
+        },
+      },
     ],
-    theme: {
-        extend: {},
-    },
-    daisyui: {
-        themes: [
-            {
-                "Spiker": {
-                    ...require("daisyui/src/theming/themes")["dim"],
-                    primary: "#6ba672",
-                },
-                "Ghostiny": {
-                    ...require("daisyui/src/theming/themes")["emerald"],
-                },
-            },
-        ],
-        darkTheme: "Spiker",
-        logs: false,
-    },
+    darkTheme: 'Spiker',
+    logs: false,
+  },
 };
